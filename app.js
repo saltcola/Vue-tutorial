1a44c4bc2965970478540223beb7ec8df42dfa7c
Gang Fan <gang.fan@uconn.edu>
2016-08-18 13:19:58 -0400

    Adding Event Data & Adding Event Data

--- app.js
+++ app.js
@@ -7,11 +7,54 @@ new Vue({
 
   // Here we can register any values or collections that hold data
   // for the application
-  data: {},
+  data: {
+  event: { name: '', description: '', date: '' },
+  events: []
+},
 
-  // Anything within the ready function will run when the application loads
-  ready: function() {},
+// Anything within the ready function will run when the application loads
+ready: function() {
+  // When the application loads, we want to call the method that initializes
+  // some data
+  this.fetchEvents();
+},
 
-  // Methods we want to use in our application are registered here
-  methods: {}
+// Methods we want to use in our application are registered here
+methods: {
+
+  // We dedicate a method to retrieving and setting some data
+  fetchEvents: function() {
+    var events = [
+      {
+        id: 1,
+        name: 'TIFF',
+        description: 'Toronto International Film Festival',
+        date: '2015-09-10'
+      },
+      {
+        id: 2,
+        name: 'The Martian Premiere',
+        description: 'The Martian comes to theatres.',
+        date: '2015-10-02'
+      },
+      {
+        id: 3,
+        name: 'SXSW',
+        description: 'Music, film and interactive festival in Austin, TX.',
+        date: '2016-03-11'
+      }
+    ];
+    // $set is a convenience method provided by Vue that is similar to pushing
+    // data onto an array
+    this.$set('events', events);
+  },
+
+  // Adds an event to the existing events array
+  addEvent: function() {
+    if(this.event.name) {
+      this.events.push(this.event);
+      this.event = { name: '', description: '', date: '' };
+    }
+  }
+}
 });
