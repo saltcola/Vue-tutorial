872d2154474a5832fec6167b52ce7a01cc9b7f28
Gang Fan <gang.fan@uconn.edu>
2016-08-19 09:43:42 -0400

    Switching to a Back End

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // app.js
-
+/*
 new Vue({
 
     // We want to target the div with an id of 'events'
@@ -24,40 +24,106 @@ new Vue({
 
         // We dedicate a method to retrieving and setting some data
         fetchEvents: function() {
-            var events = [{
-                id: 1,
-                name: 'TIFF',
-                description: 'Toronto International Film Festival',
-                date: '2015-09-10'
-            }, {
-                id: 2,
-                name: 'The Martian Premiere',
-                description: 'The Martian comes to theatres.',
-                date: '2015-10-02'
-            }, {
-                id: 3,
-                name: 'SXSW',
-                description: 'Music, film and interactive festival in Austin, TX.',
-                date: '2016-03-11'
-            }];
-            // $set is a convenience method provided by Vue that is similar to pushing
-            // data onto an array
-            this.$set('events', events);
+            var events = [];
+      // this.$set('events', events);
+	      this.$http.get('/api/events')
+	        .success(function (events) {
+	          this.$set('events', events);
+	          console.log(events);
+	        })
+	        .error(function (err) {
+	          console.log(err);
+	        });
         },
 
         // Adds an event to the existing events array
         addEvent: function() {
-            if (this.event.name) {
-                this.events.push(this.event);
-                this.event = { name: '', description: '', date: '' };
-            }
+            if (this.event.title.trim()) {
+		        // this.events.push(this.event);
+		        // this.event = { title: '', detail: '', date: '' };
+		        this.$http.post('/api/events', this.event)
+		          .success(function (res) {
+		            this.events.push(this.event);
+		            console.log('Event added!');
+		          })
+		          .error(function (err) {
+		            console.log(err);
+		          });
+		      }
         }，
 
          deleteEvent: function(index) {
-            if (confirm("Are you sure you want to delete this event?")) {
-                // $remove is a Vue convenience method similar to splice
-                this.events.$remove(index);
+            if (confirm('確定要移除此項事件？')) {
+	        // this.events.splice(index, 1);
+	        this.$http.delete('api/events/' + event.id)
+	          .success(function (res) {
+	            console.log(res);
+	            this.events.splice(index, 1);
+	          })
+	          .error(function (err) {
+	            console.log(err);
+	          });
             }
         }
     }
+});
+*/
+
+
+new Vue({
+  el: '#events',
+
+  data: {
+    event: { title: '', detail: '', date: '' },
+    events: []
+  },
+
+  ready: function () {
+    this.fetchEvents();
+  },
+
+  methods: {
+
+    fetchEvents: function () {
+      var events = [];
+      // this.$set('events', events);
+      this.$http.get('/api/events')
+        .success(function (events) {
+          this.$set('events', events);
+          console.log(events);
+        })
+        .error(function (err) {
+          console.log(err);
+        });
+    },
+
+    addEvent: function () {
+      if (this.event.title.trim()) {
+        // this.events.push(this.event);
+        // this.event = { title: '', detail: '', date: '' };
+        this.$http.post('/api/events', this.event)
+          .success(function (res) {
+            this.events.push(this.event);
+            console.log('Event added!');
+          })
+          .error(function (err) {
+            console.log(err);
+          });
+      }
+    },
+
+    deleteEvent: function (index) {
+      if (confirm('確定要移除此項事件？')) {
+        // this.events.splice(index, 1);
+        this.$http.delete('api/events/' + event.id)
+          .success(function (res) {
+            console.log(res);
+            this.events.splice(index, 1);
+          })
+          .error(function (err) {
+            console.log(err);
+          });
+      }
+    }
+  }
 });
--- readme.md
+++ readme.md
@@ -1,1 +1,17 @@
-##[Build an App with Vue.js: A Lightweight Alternative to AngularJS](https://scotch.io/tutorials/build-an-app-with-vue-js-a-lightweight-alternative-to-angularjs)##
+#[Build an App with Vue.js: A Lightweight Alternative to AngularJS](https://scotch.io/tutorials/build-an-app-with-vue-js-a-lightweight-alternative-to-angularjs)
+
+## Vue Events Bulletin Board
+
+This is the code for the Vue.js [tutorial on Scotch.io](https://scotch.io/tutorials/build-a-single-page-time-tracking-app-with-vue-js-introduction). In the tutorial we build a events bulletin board application and cover the basics of [Vue](http://vuejs.org/).
+
+## Installation
+
+1. Run `npm install`.
+2. Run `node server.js`.
+3. Visit [http://localhost:8080](http://localhost:8080).
+
+## RESTful API (contributed by Jason Lam)
+
+1. **Use Node.js & Express for backend server and router.**
+2. **RESTful requests towards the server to simulate CRUD on *events* model, instead of local hardcoded ones.**
+3. Translated into Traditional Chinese.
